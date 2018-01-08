6b3d7d9ff97b403430255d6ed7e5bd96c5e08933
Alexandre Caron DESKTOP <alexcaron880@gmail.com>
2018-01-08 13:32:31 -0500

    trying

--- cours17notes/Program.cs
+++ cours17notes/Program.cs
@@ -8,8 +8,59 @@ namespace cours17notes
 {
     class Program
     {
+        static bool TryParse(string s, out int val) // tryparse
+        {
+            try
+            {
+                val = int.Parse(s);
+                return true;
+            }
+            catch
+            {
+                val = 0;
+                return false;
+            }
+        }
+        static int resultat = 0;
         static void Main(string[] args)
         {
+
+            {
+                int res;
+                // modulo surtout pour nombres entiers
+                // "Hello"[2] == l
+                string s = "172";
+                bool success = TryParse("suif", out res);
+
+                Console.WriteLine($"{success},{res}");
+                //int x = Convert.ToInt32(Console.ReadLine()); // conversion de whatever en int3
+                //int.Parse(s);
+                {
+
+                    foreach (char c in s)
+                    {
+                        resultat *= 10;
+                        switch (c)
+                        {
+                            case '0':
+                                resultat += 0; break;
+                            case '1': resultat += 1; break;
+                            case '2': resultat += 2; break;
+                            case '3': resultat += 3; break;
+                            case '4': resultat += 4; break;
+                            case '5': resultat += 5; break;
+                            case '6': resultat += 6; break;
+                            case '7': resultat += 7; break;
+                            case '8': resultat += 8; break;
+                            case '9': resultat += 9; break;
+                        }
+                        Console.WriteLine($"{resultat}");
+                    }
+
+                    int z;
+                    if (int.TryParse((s), out z) == true) { Console.WriteLine($"{z} et {resultat}"); }
+                }
+            }
         }
     }
 }
---
+++ cours17wpfcalc/MainWindow.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cours17wpfcalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        static void EntryCNT(ref int x, ref string[] y, char z)
+        {
+            Calcx[NewEntry] = z;
+            NewEntry++;
+            //LblRes.Content += z;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+             public static string[] Calcx;
+        static int NewEntry = 0;
+
+    
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            
+            EntryCNT(ref NewEntry,ref Calcx, '1');
+           // LblRes.Content += "1";
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Calcx[NewEntry] = "2";
+            NewEntry++;
+            LblRes.Content += "2";
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            Calcx[NewEntry] = "3";
+            NewEntry++;
+            LblRes.Content += "3";
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            Calcx[NewEntry] = "4";
+            NewEntry++;
+            LblRes.Content += "4";
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            Calcx[NewEntry] = "5";
+            NewEntry++;
+            LblRes.Content += "5";
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            Calcx[NewEntry] = "6";
+            NewEntry++;
+            LblRes.Content += "6";
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+            Calcx[NewEntry] = "7";
+            NewEntry++;
+            LblRes.Content += "7";
+        }
+
+        private void Button8_Click(object sender, RoutedEventArgs e)
+        {
+            Calcx[NewEntry] = "8";
+            NewEntry++;
+            LblRes.Content += "8";
+        }
+
+        private void Button9_Click(object sender, RoutedEventArgs e)
+        {
+            Calcx[NewEntry] = "9";
+            NewEntry++;
+            LblRes.Content += "9";
+        }
+
+        private void Button0_Click(object sender, RoutedEventArgs e)
+        {
+            Calcx[NewEntry] = "0";
+            NewEntry++;
+            LblRes.Content += "0";
+        }
+
+        private void ButtonDot_Click(object sender, RoutedEventArgs e)
+        {
+            Calcx[NewEntry] = ".";
+            NewEntry++;
+            LblRes.Content += ".";
+        }
+
+        private void ButtonPlus_Click(object sender, RoutedEventArgs e)
+        {
+            Calcx[NewEntry] = "+";
+            NewEntry++;
+            LblRes.Content += "+";
+        }
+
+        private void ButtonMinus_Click(object sender, RoutedEventArgs e)
+        {
+            Calcx[NewEntry] = "-";
+            NewEntry++;
+            LblRes.Content += "-";
+        }
+
+        private void ButtonTime_Click(object sender, RoutedEventArgs e)
+        {
+            Calcx[NewEntry] = "*";
+            NewEntry++;
+            LblRes.Content += "*";
+        }
+
+        private void ButtonDivide_Click(object sender, RoutedEventArgs e)
+        {
+            Calcx[NewEntry] = "/";
+            NewEntry++;
+            LblRes.Content += "/";
+        }
+
+        private void ButtonEqual_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonPlusMinus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
